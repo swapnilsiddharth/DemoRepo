3a1dc9076f35d8051ab582f821aacccfa36b616f
swapnil <swapnil.sidd@gmail.com>
2019-08-01 22:26:23 +0530

    first commit

---
+++ src/gitdemo/FirstDemo.java
@@ -0,0 +1,18 @@
+package gitdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\demo\\Downloads\\sidd\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		//driver.close();
+	}
+
+}
